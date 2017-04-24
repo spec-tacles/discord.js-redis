94a3a4e90eb1681cdcd5b1cc53c34248b021a3c4
Will Nelson <nelson.will@live.com>
2017-04-24 11:36:38 -0700

    testing

--- test/index.js
+++ test/index.js
@@ -1,7 +1,15 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+const assert = require('assert');
 const redis = require('../src/index.js')();
 const discord = require('discord.js');
 
 const client = new discord.Client();
-redis(client);
-client.login(process.env.DISCORD_TOKEN);
+describe('initialization', function() {
+  it('should initialize the redis interface without errors', function() {
+    redis(client);
+  });
+  it('should login without errors', function() {
+    return client.login(process.env.DISCORD_TOKEN);
+  });
+});
