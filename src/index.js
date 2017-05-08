d90f1e0aa528e68fdf1c41d356b6797621dcd427
Hackzzila <admin@hackzzila.com>
2017-05-08 18:15:29 -0500

    Cleanup

--- src/index.js
+++ src/index.js
@@ -22,11 +22,20 @@ module.exports = (options) => {
       return Promise.all(messages.map(m => q.hdelAsync(`message:${m.id}`)))
         .then(() => q.execAsync());
     });
+
     client.on('userUpdate', (o, n) => r.setUser(n));
+    
+    client.on('channelCreate', c => r.setChannel(c));
     client.on('channelUpdate', (o, n) => r.setChannel(n));
+    client.on('channelDelete', c => r.deleteChannel(c));
+
+    client.on('emojiCreate', e => r.setEmoji(e));
     client.on('emojiUpdate', (o, n) => r.setEmoji(n));
+    client.on('emojiDelete', e => r.deleteEmoji(e));
+
+    client.on('guildCreate', g => r.setGuild(g));
     client.on('guildUpdate', (o, n) => r.setGuild(n));
-    client.on('messageUpdate', (o, n) => r.setMessage(n));
+    client.on('guildDelete', g => r.deleteGuild(g));
 
     return r;
   }, { redis: r });
