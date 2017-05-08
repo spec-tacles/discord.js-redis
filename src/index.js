5ea67e12b2caa0a1e7dfc1092dc33ae04725bc3d
Will Nelson <nelson.will@live.com>
2017-05-08 16:27:22 -0700

    better exporting

--- src/index.js
+++ src/index.js
@@ -1,42 +1,32 @@
 const ClientDataManager = require('./ClientDataManagerExtension');
 const RedisInterface = require('./RedisInterface');
 
-module.exports = (options) => {
+module.exports = (client, options) => {
   const r = new RedisInterface(options);
-  return Object.assign((client) => {
-    // eslint-disable-next-line no-param-reassign
-    client.dataManager = new ClientDataManager(client, r);
-    client.once('ready', () => {
-      const q = r.client.multi();
-      const queries = client.users.map(u => q.hmsetAsync(`user:${u.id}`, RedisInterface.clean(u)));
-      queries.push(...client.channels.map(c => q.hmsetAsync(`channel:${c.id}`, RedisInterface.clean(c))));
-      queries.push(...client.guilds.map(g => q.hmsetAsync(`guild:${g.id}`, RedisInterface.clean(g))));
-      queries.push(...client.emojis.map(e => q.hmsetAsync(`emoji:${e.id}`, RedisInterface.clean(e))));
-      return Promise.all(queries).then(() => q.execAsync());
-    });
-    client.on('message', r.setMessage.bind(this));
-    client.on('messageDelete', r.deleteMessage.bind(this));
-    client.on('messageUpdate', (o, n) => r.setMessage(n));
-    client.on('messageDeleteBulk', (messages) => {
-      const q = r.client.multi();
-      return Promise.all(messages.map(m => q.hdelAsync(`message:${m.id}`)))
-        .then(() => q.execAsync());
-    });
 
-    client.on('userUpdate', (o, n) => r.setUser(n));
-    
-    client.on('channelCreate', c => r.setChannel(c));
-    client.on('channelUpdate', (o, n) => r.setChannel(n));
-    client.on('channelDelete', c => r.deleteChannel(c));
+  // eslint-disable-next-line no-param-reassign
+  client.dataManager = new ClientDataManager(client, r);
+  client.once('ready', () => {
+    const q = r.client.multi();
+    const queries = client.users.map(u => q.hmsetAsync(`user:${u.id}`, RedisInterface.clean(u)));
+    queries.push(...client.channels.map(c => q.hmsetAsync(`channel:${c.id}`, RedisInterface.clean(c))));
+    queries.push(...client.guilds.map(g => q.hmsetAsync(`guild:${g.id}`, RedisInterface.clean(g))));
+    queries.push(...client.emojis.map(e => q.hmsetAsync(`emoji:${e.id}`, RedisInterface.clean(e))));
+    return Promise.all(queries).then(() => q.execAsync());
+  });
+  client.on('message', r.setMessage.bind(this));
+  client.on('messageDelete', r.deleteMessage.bind(this));
+  client.on('messageUpdate', (o, n) => r.setMessage(n));
+  client.on('messageDeleteBulk', (messages) => {
+    const q = r.client.multi();
+    return Promise.all(messages.map(m => q.hdelAsync(`message:${m.id}`)))
+      .then(() => q.execAsync());
+  });
+  client.on('userUpdate', (o, n) => r.setUser(n));
+  client.on('channelUpdate', (o, n) => r.setChannel(n));
+  client.on('emojiUpdate', (o, n) => r.setEmoji(n));
+  client.on('guildUpdate', (o, n) => r.setGuild(n));
+  client.on('messageUpdate', (o, n) => r.setMessage(n));
 
-    client.on('emojiCreate', e => r.setEmoji(e));
-    client.on('emojiUpdate', (o, n) => r.setEmoji(n));
-    client.on('emojiDelete', e => r.deleteEmoji(e));
-
-    client.on('guildCreate', g => r.setGuild(g));
-    client.on('guildUpdate', (o, n) => r.setGuild(n));
-    client.on('guildDelete', g => r.deleteGuild(g));
-
-    return r;
-  }, { redis: r });
+  return r;
 };
